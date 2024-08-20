58b252b97d15a6878c0b84f2a7d2b3a156e31b0a
Sankalpa0011 <sankalpakavindu09@gmail.com>
2024-08-20 18:04:58 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ AUTHOR_USER_NAME = "Sankalpa0011"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "sankalpakavindu09@gmail.com"
 
-
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="a small python package for CNN app",
-    long_description_content="text/markdown",
-    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    projecct_url={
+    description="A small Python package for CNN app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"", "src"},
-    packages=setuptools.find_packages(where="src")
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
 )
